0d3917b134822a66991d45eb55bb3947ad304d8b
George Dernikos <geoder101@gmail.com>
2015-08-09 14:05:49 +0300

    help message

---
+++ help.mk
@@ -0,0 +1,13 @@
+MK_HELP_CMD_PADDING ?= 25
+
+help:
+	printf "Available targets:\n\n"
+	awk '/^[a-zA-Z\-\_0-9]+:/ { \
+		helpMessage = match(lastLine, /^## (.*)/); \
+		if (helpMessage) { \
+			helpCommand = substr($$1, 0, index($$1, ":")-1); \
+			helpMessage = substr(lastLine, RSTART + 3, RLENGTH); \
+			printf "%-$(MK_HELP_CMD_PADDING)s %s\n", helpCommand, helpMessage; \
+		} \
+	} \
+	{ lastLine = $$0 }' $(MAKEFILE_LIST)
