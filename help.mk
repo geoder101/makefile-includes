bc892d6a5f2700bbbc6da4f9a1f65fcce2700837
George Dernikos <geoder101@gmail.com>
2015-08-15 13:18:14 +0300

    silent help msg

--- help.mk
+++ help.mk
@@ -1,8 +1,8 @@
 MK_HELP_CMD_PADDING ?= 25
 
 help:
-	printf "Available targets:\n\n"
-	awk '/^[a-zA-Z\-\_0-9]+:/ { \
+	@printf "Available targets:\n\n"
+	@awk '/^[a-zA-Z\-\_0-9]+:/ { \
 		helpMessage = match(lastLine, /^## (.*)/); \
 		if (helpMessage) { \
 			helpCommand = substr($$1, 0, index($$1, ":")-1); \
